2bcd90f6f0259597b3bc9144f2eff44bc0d2d1a9
Harshal Paltse <148197108+harshal-paltse@users.noreply.github.com>
2026-08-22 16:07:40 +0530

    feat(android): configure multi-module android gradle environment and dependency injection settings

---
+++ android/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://plaid.github.io/link-android/") }
+    }
+}
+
+rootProject.name = "TrustMesh"
+include(":app")
+include(":design-system")
+include(":domain")
+include(":data")
